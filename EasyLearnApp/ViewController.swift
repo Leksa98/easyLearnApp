7a61c20bd6f210dcdfd0f23c5afa68589035b6cd
Alexandra Gertsenshtein <alexandra@Alexandras-MacBook-Pro.local>
2020-06-29 18:31:24 +0300

    Project start

---
+++ EasyLearnApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyLearnApp
+//
+//  Created by Alexandra Gertsenshtein on 29.06.2020.
+//  Copyright © 2020 Alexandra Gertsenshtein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
