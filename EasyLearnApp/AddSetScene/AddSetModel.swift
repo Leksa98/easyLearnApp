7c703c597436a82c35dcd3c9b4b36fab00b98099
Alexandra Gertsenshtein <aleksa300798@gmail.com>
2020-07-19 22:50:54 +0300

    Add Clean Swift architecture

---
+++ EasyLearnApp/AddSetScene/AddSetModel.swift
@@ -0,0 +1,19 @@
+//
+//  AddSetModel.swift
+//  EasyLearnApp
+//
+//  Created by Alexandra Gertsenshtein on 19.07.2020.
+//  Copyright © 2020 Alexandra Gertsenshtein. All rights reserved.
+//
+
+import Foundation
+
+final class AddSetModel {
+    var word: String
+    var translation: String
+    
+    init(word: String, translation: String) {
+        self.word = word
+        self.translation = translation
+    }
+}
